7ab7071a6d84996c4c33d9b32c117d5c50a254db
Marija273 <maka.totovic@gmail.com>
2022-06-01 20:12:47 +0200

    U index.php postavljen naslov i dugme koje otvara Bootstrap modalnu formu za unos novog ispita, css u index.css

---
+++ add-modal.php
@@ -0,0 +1,38 @@
+<div class="modal fade" id="modal-dodaj-ispit" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Novi ispit</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div>
+                    <label>Naziv:</label>
+                    <input type="text" class="form-control" id="naziv">
+                </div>
+                <div>
+                    <label>ESPB: </label>
+                    <input type="number" class="form-control" id="espb">
+                </div>
+                <div>
+                    <label>Semestar: </label>
+                    <input type="text" class="form-control" id="semestar">
+                </div>
+                <div>
+                    <label>Smer: </label>
+                    <input type="text" class="form-control" id="smer">
+                </div>
+                <div>
+                    <label>Katedra: </label>
+                    <select class="form-select" id="katedra">
+
+                    </select>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary">Dodaj</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Zatvori</button>
+            </div>
+        </div>
+    </div>
+</div>
